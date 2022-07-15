8aba022030c354128a2fa0f62aff648ec0b3c5a2
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2022-07-15 18:28:24 +0900

    Cros設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,12 +44,19 @@ export default {
   modules: [
     // https://go.nuxtjs.dev/axios
     '@nuxtjs/axios',
+    '@nuxtjs/proxy',
   ],
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     baseURL: '/',
+  },
+
+  proxy: {
+    '/api': {
+      target: 'http://localhost:3000/'
+    }
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build

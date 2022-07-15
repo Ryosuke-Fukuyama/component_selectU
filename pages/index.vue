145a7646dd2cded4fdfc089fde0e8ade215cd35e
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2022-07-15 18:04:36 +0900

    axios動作確認

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,27 @@
 <template>
-  <status_change></status_change>
+  <section>
+    <!-- <status_change></status_change> -->
+    <button @click="getApi()">GET API</button>
+    {{ message }}
+  </section>
 </template>
 
 <script>
-export default {
-  name: 'IndexPage'
-}
+  export default {
+    data: () => ({
+      message: ''
+    }),
+    methods: {
+      getApi() {
+          const url = "/api/v1/hello"
+          this.$axios.get(url)
+            .then((res) => {
+              this.message = res.data
+            })
+            .catch((error) => {
+              console.log(error)
+            })
+        }
+    }
+  }
 </script>

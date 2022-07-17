aaa29a3c8f77a929b55fc846148ee7e5044abc70
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2022-07-17 10:31:21 +0900

    Nuxt側updateメソッド

--- pages/properties/index.vue
+++ pages/properties/index.vue
@@ -3,6 +3,12 @@
     <ul>
       <li v-for="property in properties" :key="property.id">
         {{ property.content }}
+        <select  @click="update()">
+          <option disabled value="">{{ property.content }}</option>
+          <option>"content A"</option>
+          <option>"content B"</option>
+          <option>"content C"</option>
+        </select>
       </li>
     </ul>
   </div>
@@ -11,12 +17,24 @@
 <script>
 export default {
   data:() => ({
-    properties: []
+    properties: [],
+    property: {},
+    content: ''
   }),
 
 // ページがマウントされたタイミングでfetchContents()を実行
   mounted() {
     this.fetchContents()
+  },
+
+  computed: {
+    params() {
+      return {
+        property: {
+          content: this.content
+        }
+      }
+    },
   },
 
   methods: {
@@ -31,6 +49,16 @@ export default {
         //   // リクエスト失敗時の処理
         // })
         this.properties = res.data.properties
+        })
+    },
+    update() {
+      const url = `/api/v1/properties/${this.$route.params.id}`
+      this.$axios.put(url, this.params)
+        .then((res) => {
+          // 成功時の処理
+        })
+        .catch((err) => {
+          // 失敗時の処理
         })
     }
   }

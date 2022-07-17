79442cd695b955572811654c038e7a700573b26c
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2022-07-17 10:06:03 +0900

    コンテンツ一覧表示

---
+++ pages/properties/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="property in properties" :key="property.id">
+        {{ property.content }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data:() => ({
+    properties: []
+  }),
+
+// ページがマウントされたタイミングでfetchContents()を実行
+  mounted() {
+    this.fetchContents()
+  },
+
+  methods: {
+    fetchContents() {
+      const url = "/api/v1/properties"
+      this.$axios.get(url)
+        .then((res) => {
+        //   // リクエスト成功時の処理
+        //   console.log(res)
+        // })
+        // .catch(() => {
+        //   // リクエスト失敗時の処理
+        // })
+        this.properties = res.data.properties
+        })
+    }
+  }
+}
+</script>

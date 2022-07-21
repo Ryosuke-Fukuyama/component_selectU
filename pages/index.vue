c9b113dddfe6a5f00e093a921b156a1a0fcd881d
ryosuke fukuyama <ryosukemakaritoru425@gmail.com>
2022-07-21 22:48:02 +0900

    [change] # session検証も兼ねてリテイク/API連携確認

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="text-center">
+    <button @click="getApi()">GET API</button>
+    <div>{{ message }}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data: () => {
+    return {
+      message: ''
+    }
+  },
+
+  methods: {
+    getApi() {
+      const url = "/api/v1/hello"
+      this.$axios.get(url)
+        .then((res) => {
+          this.message = res.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+  }
+}
+</script>
